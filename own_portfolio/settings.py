3909589cd9669e09b7d6bd4b4ee77df3a98257d5
Hokkkkkkk <hodetskiyrostislav@gmail.com>
2024-01-30 13:36:02 +0000

    Remove old stuff

--- own_portfolio/settings.py
+++ own_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--@#*fyi%_sb2u)uy8p+wp)789leo3=5c32&*si#&l1u4p4w-2&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eque.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
